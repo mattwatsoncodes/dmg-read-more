c5732d401ba2105bfdf34add400950fa31b0a58e
Matt Watson <matt@mattwatson.codes>
2025-02-09 00:03:22 +0000

    Import dev repository (with full history) and build assets for release 0.2.1

--- build/dmg-read-more/index.asset.php
+++ build/dmg-read-more/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7fb3a92c9632a4c680fd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'c8fb6201024021f4af6b');
